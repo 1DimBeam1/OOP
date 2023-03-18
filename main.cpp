8ae56f187ab815e4d093a3151606d1ea4900768c
1DimBeam1 <pro100dima_2016@mail.ru>
2023-03-18 12:01:53 +1000

    Clear branch

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
-#include "ClassModule.h"
+
 int main() {
-    Character A(1,"BoB"), B(2,"GoGi");
-    Administrator god("S+");
-    int id;
-
-    A.print();
-    cout << "/////////////////////////////////\n";
-    id = A.get_ID();
-    god.GoodBoy(&A,id);
-    cout << "/////////////////////////////////\n";
-    A.print();
-
     return 0;
 }
